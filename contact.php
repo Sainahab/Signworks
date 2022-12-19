4cd98bd30a607dad4a0efa58ede6b83a379475d2
Sain Ahab <dvlproject.hq@gmail.com>
2022-12-19 17:03:42 +0100

    updated
    -fixed form
    -added google analytics
    - fixed map
    -added new team member

--- contact.php
+++ contact.php
@@ -1,28 +1,43 @@
 <?php
-
+/*
+ *  CONFIGURE EVERYTHING HERE
+ */
 
 // an email address that will be in the From field of the email.
-$from = 'info@signworks.ma';
+$from = 'www.signworks.ma';
 
+// an email address that will receive the email with the output of the form
 $sendTo = 'info@signworks.ma';
 
-$subjecto = 'New message from contact form';
-$fields = array('name' => 'Name', 'subject' => 'subject', 'need' => 'Need', 'email' => 'email', 'message' => 'message'); 
+// subject of the email
+$subject = 'New message from www.Signworks.ma Contact Form';
 
-$okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
+// form field names and their translations.
+// array variable name => Text to appear in the email
+$fields = array('name' => 'Name', 'subject' => 'Subject', 'need' => 'Need', 'email' => 'Email', 'message' => 'Message'); 
 
-$errorMessage = 'There was an error while submitting the form. Please try again later';
-error_reporting(0);
+// message that will be displayed when everything is OK :)
+$okMessage = 'Your message successfully Delivered. Thank you, We will get back to you soon!';
+
+// If something goes wrong, we will display this message.
+$errorMessage = 'There was an error delivering your message. Please try again later';
+
+/*
+ *  LET'S DO THE SENDING
+ */
+
+// if you are not debugging and don't need error reporting, turn this off by error_reporting(0);
+error_reporting(E_ALL & ~E_NOTICE);
 
 try
 {
 
     if(count($_POST) == 0) throw new \Exception('Form is empty');
             
-    $emailText = "You have a new message from your contact form\n=============================\n";
+    $emailText = "You have a new message from your (www.signworks.ma) contact form \n=============================\n";
 
     foreach ($_POST as $key => $value) {
-
+        // If the field exists in the $fields array, include it in the email 
         if (isset($fields[$key])) {
             $emailText .= "$fields[$key]: $value\n";
         }
@@ -36,7 +51,7 @@ try
     );
     
     // Send email
-    mail($sendTo, $subjecto, $emailText, implode("\n", $headers));
+    mail($sendTo, $subject, $emailText, implode("\n", $headers));
 
     $responseArray = array('type' => 'success', 'message' => $okMessage);
 }
@@ -54,6 +69,7 @@ if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQ
 
     echo $encoded;
 }
+// else just display the message
 else {
     echo $responseArray['message'];
 }
